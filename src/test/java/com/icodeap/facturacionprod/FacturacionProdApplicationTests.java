c6df000c6ba7cbf46958a45769cce7103be3d01a
Víctor <vjtalavera@gmail.com>
2025-07-21 10:22:58 +0100

    Configuracion proyecto, acceso a bd

---
+++ src/test/java/com/icodeap/facturacionprod/FacturacionProdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icodeap.facturacionprod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacturacionProdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
